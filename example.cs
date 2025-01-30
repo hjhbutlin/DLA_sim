1f1249b1ec0b22fafcae1ea0a046d023be0b10e5
Henry Butlin <henrybutlin@Henrys-MacBook.local>
2025-01-30 11:43:04 +0000

    Initial commit

---
+++ example.cs
@@ -0,0 +1,132 @@
+﻿// TODO
+// random position on edge of window rather than anywhere maybe
+// colour to represent particle time to aggregate
+// properly parameterise scale
+// show diffusion path
+
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Particle {
+    public Vector2i Position;
+    public Particle(int x, int y) {
+        Position = new Vector2i(x, y);
+    }
+}
+
+public class Simulation {
+    private const int WindowWidth = 300;
+    private const int WindowHeight = 200;
+    private const int ParticleCount = 1000;
+
+    private RenderWindow window;
+    private List<Particle> particles;
+    private HashSet<Vector2i> occupiedPositions;
+
+    public Simulation() {
+        window = new RenderWindow(new VideoMode((uint)WindowWidth, (uint)WindowHeight), "DLA Simulation");
+        window.Closed += (sender, e) => window.Close();
+
+        particles = new List<Particle>();
+        occupiedPositions = new HashSet<Vector2i>();
+
+        var origin = new Particle(WindowWidth / 10, WindowHeight / 10);
+        particles.Add(origin);
+        occupiedPositions.Add(origin.Position);
+    }
+
+    private void Diffuse(Particle particle) {
+
+        while (! IsAdjacent(particle.Position)) {
+            int dx = RandomWithEdgeCase(particle.Position, true); // true => x edge case
+            int dy = RandomWithEdgeCase(particle.Position, false); // false +> y case
+
+            particle.Position = new Vector2i(particle.Position.X + dx, particle.Position.Y + dy);
+            System.Console.WriteLine(particle.Position);
+            }
+        // once particle is adjacent to another particle, it remains there.
+        occupiedPositions.Add(particle.Position);
+        particles.Add(new Particle(particle.Position.X, particle.Position.Y));
+        }
+
+    private bool IsAdjacent(Vector2i position) {
+        // 8 positions to check
+        foreach (var offset in new[] { new Vector2i(-1, -1), new Vector2i(0, -1), new Vector2i(1, -1),
+                                       new Vector2i(-1, 0), new Vector2i(1, 0),
+                                       new Vector2i(-1, 1), new Vector2i(0, 1), new Vector2i(1, 1) })
+        {
+            var neighbor = position + offset;
+            if (occupiedPositions.Contains(neighbor))
+                return true;
+        }
+        return false;
+    }
+
+    private int RandomWithEdgeCase(Vector2i position, bool x) {
+
+        Random random = new Random();
+
+        if (x) {
+            if (position.X <= 0) {
+                return 1;
+                } else if (position.X >= WindowWidth) {
+                    return -1;
+                }
+            }
+
+            else {
+                if (position.Y <= 0) {
+                return 1;
+                } else if (position.Y >= WindowHeight) {
+                    return -1;
+                }
+            }
+
+        return random.Next(-1,2);
+        }
+
+
+    public void Run() {
+        int particleScale = 5;
+        Random random = new Random();
+
+        while (window.IsOpen)
+        {
+            window.DispatchEvents();
+            window.Clear();
+
+            if (particles.Count < ParticleCount)
+            {
+                // particle at edge of window
+                var x = random.Next(0, WindowWidth/5);
+                var y = random.Next(0, WindowHeight/5);
+
+                var newParticle = new Particle(x, y);
+                Diffuse(newParticle);
+            }
+
+            // draw all the particles as 1x1 pixels
+            foreach (var particle in particles)
+            {
+                var shape = new RectangleShape(new Vector2f(particleScale, particleScale))
+                {
+                    Position = new Vector2f(particle.Position.X * particleScale, particle.Position.Y * particleScale),
+                    FillColor = Color.Red
+                };
+                window.Draw(shape);
+            }
+
+            window.Display();
+        }
+    }
+
+    public static void Main()
+    {
+        var simulation = new Simulation();
+        simulation.Run();
+    }
+}
